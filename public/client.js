8831c690fdbf52262e365593c87791ccacbe2748
nonderbe <niels.onderbeke@gmail.com>
2025-05-06 14:44:24 +0200

    Update client.js
    Chunksize verdubbeld naar 128kB

--- public/client.js
+++ public/client.js
@@ -604,7 +604,7 @@ function sendFileWithProgress(file, fileId = Date.now().toString()) {
     return;
   }
 
-  const chunkSize = 65536;
+  const chunkSize = 131072;
   file.arrayBuffer().then(buffer => {
     const totalSize = buffer.byteLength;
     transfers.set(fileId, {
